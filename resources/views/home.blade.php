155b68d94c51c22f9bfe2e1628d8e2ab77e3e48a
carlosdmx3 <carlos.sanchezeiem.gob.mx>
2024-01-18 12:19:03 -0600

    pruebas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -124,7 +124,7 @@
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="radio" class="form-check-input" name="odepartamentoct">
-                                Generales
+                                Generales.
                             </label>
                         </div>
                         <div class="form-check">
